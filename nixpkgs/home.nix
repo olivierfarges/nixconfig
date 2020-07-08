b946dfe4d25bcf91db4189e9bbfbf03b2d57653b
btwiusegentoo <66811008+btwiusegentoo@users.noreply.github.com>
2020-07-08 09:49:11 +0900

    :heavy_plus_sign: Add fish shell plugin(z), fix vi key bindings.

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -65,6 +65,8 @@ in
         #fish{{{
         fish = {
             enable = true;
+
+            #alias{{{
             shellAbbrs = {
                 #Common commands
                 "c" = "clear";
@@ -99,12 +101,28 @@ in
                 "chome" = "nvim ~/mygit/nixconfig/nixpkgs/home.nix";
                 "cnix" = "nvim ~/mygit/nixconfig/configuration.nix";
             };
+# }}}
+
+            #plugins
+            plugins =
+                [
+                    {
+                        name = "z";
+                        src = pkgs.fetchFromGitHub {
+                            owner = "jethrokuan";
+                            repo = "z";
+                            rev = "ddeb28a7b6a1f0ec6dae40c636e5ca4908ad160a";
+                            sha256 = "0c5i7sdrsp0q3vbziqzdyqn4fmp235ax4mn4zslrswvn8g3fvdyh";
+                        };
+                    }
+                ];
 
             interactiveShellInit =
             ''
-                set fish_vi_key_bindings
+                fish_vi_key_bindings
                 set fish_greeting
             '';
+
         };
         #}}}
 
